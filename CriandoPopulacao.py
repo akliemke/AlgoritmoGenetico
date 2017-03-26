6a9eb141f9be0fb259136b1d01177fa273d70ce3
Albert Kliemke <albertkliemke@gmail.com>
2017-03-26 01:15:52 -0300

    Gerando individuos e somando o valor do mesmo.

--- CriandoPopulacao.py
+++ CriandoPopulacao.py
@@ -1,2 +1,27 @@
-#Criando a população
-print("Hello")
+import numpy as np
+
+def inicializarIndividuo(tamanho):
+    for x in range(0, tamanho):
+        print "             We're on time %d" %(x)
+
+
+
+print("iniciando inviduo")
+print("-----------------")
+#inicializarIndividuo(5)
+
+populacao = np.random.randint(2, size=(20,12))
+print populacao
+print "-------------"
+print "Individuo 00:"
+print populacao[0]
+print "-------------"
+print "Soma Individuo 00:"
+print np.sum(populacao[0])
+
+print "-------------"
+print "Individuo 01:"
+print populacao[1]
+print "-------------"
+print "Soma Individuo 01:"
+print np.sum(populacao[1])
