67d74d9526d276c9a53ed81917ddb0a06ececbe3
Albert Kliemke <albertkliemke@gmail.com>
2017-04-05 20:38:36 -0300

    Metodo de selecao roleta

---
+++ roleta.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+valoresFitness   = np.empty((0,3), float)
+
+def roleta(valoresFitness):
+    fitnessTotal = 0
+    # calcula total da populacao
+    for q in range(len(valoresFitness)):
+        fitnessTotal = fitnessTotal + valoresFitness[q][1]
+
+    for w in range(len(valoresFitness)):
+        valoresFitness[w][2] =  valoresFitness[w][1] / fitnessTotal
+
+    for e in range(len(valoresFitness)):
+        if e == 0:
+            valoresFitness[e][3] = valoresFitness[e][2] + valoresFitness[e][3]
+        else:
+            valoresFitness[e][3] = valoresFitness[e][2] + valoresFitness[e-1][3]
+
+    return fitnessTotal
