a6246b068cdc348c25a9b0b2a7b52e345df3cfee
Albert Kliemke <albertkliemke@gmail.com>
2017-06-01 00:25:37 -0300

    Nova versão

--- CriandoPopulacao.py
+++ CriandoPopulacao.py
@@ -1,20 +1,44 @@
 import numpy as np
-
+import time
+from operator import itemgetter
+# necessario para suprimir a notacao
+np.set_printoptions(suppress=True)
 print("iniciando inviduo")
 print("-----------------")
 
-custos = [12.5, 14.0, 10]
+custos = [8, 12, 17]
+kilometros_viagens = [500.0, 200.0, 55.0]
+cidade_saida   = [111, 222, 333]
+cidade_destino = [444, 555, 666]
 
+#a = np.zeros(shape=(3,3))
+#b = np.zeros(shape=(3,3))
 
+#a[0][0] = 1
+#a[0][2] = 1
+#a[1][1] = 1
+#a[2][0] = 1
+#a[2][2] = 1
 
-
+#b[0][2] = 1
+#b[1][1] = 1
+#b[1][1] = 0
+#b[2][0] = 1
+#b[2][1] = 1
+#b[2][2] = 1
+#c = a * b
+#print a
+#print("-----------------")
+#print b
+#print("-----------------")
+#print c
 populacao = []
 #individuo = np.random.randint(2, size=(3,12))
 #individuo2 = np.random.randint(2, size=(3,12))
 
 
-individuo  = np.zeros(shape=(3,12))
-individuo2 = np.zeros(shape=(3,12))
+individuo  = np.zeros(shape=(3,8))
+individuo2 = np.zeros(shape=(3,8))
 
 
 
@@ -23,30 +47,59 @@ populacao.append(individuo2)
 populacao = np.asarray(populacao)
 
 
-
+#print populacao
 
 
 #print type(populacao)
 #print populacao
 
-print "-------------"
-populacao[0][0][11] = 100.0
-populacao[0][1][11] = 200.0
-populacao[0][2][11] = 300.0
-
-populacao[1][0][11] = 400.0
-populacao[1][1][11] = 500.0
-populacao[1][2][11] = 600.0
-
-print populacao
+#print "-------------"
+#print populacao
 
 for indivi in range(2):
     for viagem in range(3):
-        for parametro in range(12):
-            print populacao[indivi][viagem][parametro]
+        for parametro in range(8):
+            if parametro == 0: # Dia da viagem
+                populacao[indivi][viagem][parametro] = time.strftime("%d")
+            if parametro == 1: # Cidade Partida
+                populacao[indivi][viagem][parametro] = cidade_destino[viagem]
+            if parametro == 2: # Cidade Destino
+                populacao[indivi][viagem][parametro] = cidade_saida[viagem]
+            if parametro == 3: # Km's viagem
+                populacao[indivi][viagem][parametro] = kilometros_viagens[viagem]
+            if parametro == 4:
+                populacao[indivi][viagem][parametro] = np.random.randint(3)
+            if parametro == 5: # Perido que a viagem sera realizada
+                populacao[indivi][  viagem][parametro] = np.random.randint(3)
+            if parametro == 6: # Numero do carro
+                populacao[indivi][viagem][parametro] = np.random.randint(3)
+            if parametro == 7: # Custo da viagem
+                populacao[indivi][viagem][parametro] = custos[int(populacao[indivi][viagem][6])] * populacao[indivi][viagem][3]
 
-#print populacao[0]
+                # = custos[int(populacao[indivi][viagem][6])] * int(populacao[indivi][viagem][3])
+                #print custos[int(populacao[indivi][viagem][6])]
+                #print populacao[indivi][viagem][3]
+
+            #print populacao[indivi][viagem][parametro]
+#custos[]
+#int(populacao[indivi][viagem][7])
 #print "-------------"
+#print type()
+#x = 1.0
+#y = 100000.0
+#print '%f' % (15.5 * 500)
+#print (15.5 * 500)
+print populacao
+soma =  np.sum(populacao[0], axis=0)
+arr = []
+for x in range(2):
+    soma =  np.sum(populacao[x], axis=0)
+    arr.append([x, soma[7], 0.0, 0.0])
+arr.sort(key=itemgetter(1), reverse=True)
+
+print arr
+print "-------------"
+print soma[7]
 #print populacao[0][0]
 #print "-------------"
 #print populacao[0][0][0]
